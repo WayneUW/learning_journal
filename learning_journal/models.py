f1e67f7f06f1b128bc5aedb0f35c2c7a57803b6d
Fukuhara <wfukuhar@SE-WFUKAHAR-MB1.local>
2015-01-26 20:08:44 -0800

    replaced my models with Maria's

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -1,7 +1,4 @@
 import datetime
-
-from cryptacular.bcrypt import BCRYPTPasswordManager as Manager    # import but bind to a different symbol
-
 from sqlalchemy import (
     Column,
     DateTime,
@@ -21,9 +18,11 @@ from sqlalchemy.orm import (
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
+from cryptacular.bcrypt import BCRYPTPasswordManager as Manager
 
-DBSession = scoped_session( sessionmaker( extension=ZopeTransactionExtension( ) ) )
-Base = declarative_base( )
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
 
 class MyModel(Base):
     __tablename__ = 'models'
@@ -33,32 +32,32 @@ class MyModel(Base):
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
 
-class Entry( Base ):
+
+class Entry(Base):
     __tablename__ = 'entries'
-    id = Column( Integer, primary_key=True )
-    title = Column( Unicode(255), unique=True, nullable=False)
-    body = Column( UnicodeText, default=u' ' )   # check the u with single quotes
-    created = Column( DateTime, default=datetime.datetime.utcnow )
-    edited = Column( DateTime, default=datetime.datetime.utcnow )    # Is there a way to do this without time?
-
-
-    # def __init__( self, title, body, edited, created ):    # self is an instance of the class for which you will need to create a method for it. Instead you use the @classmethod to pass the class object.
-    #     self.title = title
-    #     self.body = body
-    #     self.edited = edited
-    #     self.created = created
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
 
     @classmethod
-    def all(cls, session=None):    # cls is more generic instead of using the class name as the argument
-        if session is None:    # this is to help when you are using the command line
+    def all(cls, session=None):
+        """return a query with all entries, ordered by creation date reversed
+        """
+        if session is None:
             session = DBSession
-        return session.query(cls).order_by(sa.desc(cls.created)).all( )    # is the return for DBSession or just session?
+        return session.query(cls).order_by(sa.desc(cls.created)).all()
 
     @classmethod
     def by_id(cls, id, session=None):
+        """return a single entry identified by id
+
+        If no entry exists with the provided id, return None
+        """
         if session is None:
             session = DBSession
-        return session.query(cls).get(id)    # is the return for DBSession or just session?
+        return session.query(cls).get(id)
 
 
 class User(Base):
@@ -66,36 +65,12 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(Unicode(255), unique=True, nullable=False)
     password = Column(Unicode(255), nullable=False)
-    
+
     @classmethod
-    def by_name(cls, name, session=None):
-        if session is None:
-            session = DBSession
-        return DBSession.query(User).filter(cls.name == name).first()    # looking for a name, take the first one and hand it back to ourselves
-    
+    def by_name(cls, name):
+        return DBSession.query(User).filter(User.name == name).first()
+
     def verify_password(self, password):
         manager = Manager()
         return manager.check(self.password, password)
 
-    
-
-
-"""
-query 1
-def return_all( ):
-    allrows = session.query( Entry ).all( ).order_by( Entry.created) # with all the query hits the db and the results are returned
-    print allrows
-
-query 2
-def by_id( id_name ):
-    one_entry = session.query( Entry ).get( Id_name )
-    print one_entry
-
-"""
-
-"""
-Wayne TTD
--Check on the first argument for Index
--Define a method "all( )" that returns all entries in the db ordered by most recent
--Define a method "by_id" that returns a single entry by id
-"""
